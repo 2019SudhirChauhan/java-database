28f1ae5b219b3f6868f968ef9d42de2138a459fc
Sudhir_Karna <Sudhir_Karna@DESKTOP-Q1G5OEM>
2020-02-06 00:54:24 +0530

    Maven Project Added

---
+++ java_database/src/main/java/MyDbProject/java_database/App.java
@@ -0,0 +1,13 @@
+package MyDbProject.java_database;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
